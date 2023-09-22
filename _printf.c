a0b39777fca25b843316073e54a117c951b7d54a
Fateemah133 <oyenirantoyosi123@gmail.com>
2023-09-22 13:55:21 -0700

    update

---
+++ _printf.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _printf - function
+ * @char: character
+ * @format: is a character string
+ * @s:
+ */
+int _printf(const char *format, ...)
+{
+	char *str = va_arg(types, char *);
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+}
+
+/**print %**/
+
+
--- _putchar.c
+++ _putchar.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd>
 
 /**
  * _putchar - writes the character c to stdout
